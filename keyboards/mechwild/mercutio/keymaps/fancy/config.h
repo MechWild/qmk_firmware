852561140a68c2d3eea9ffc06d7bd0e145b7a5cf
Kyle McCreery <mccreery.kyle@gmail.com>
2023-07-04 16:44:16 -0400

    Updating mercutio files to match current qmk build

--- keyboards/mechwild/mercutio/keymaps/fancy/config.h
+++ keyboards/mechwild/mercutio/keymaps/fancy/config.h
@@ -14,16 +14,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
-
 #pragma once
-
-#define TAP_CODE_DELAY 10
-
-/* Define custom font */
-#define OLED_FONT_H "lib/mercutiofont.c"
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
